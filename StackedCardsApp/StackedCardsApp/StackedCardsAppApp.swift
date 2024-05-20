5e00279ce18ef10a2da175e2c855c0bf34490686
Serhat Polat <132306641+polatserhat@users.noreply.github.com>
2024-05-20 16:15:15 +0300

    Add files via upload

---
+++ StackedCardsApp/StackedCardsApp/StackedCardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackedCardsAppApp.swift
+//  StackedCardsApp
+//
+//  Created by Serhat  on 20.05.24.
+//
+
+import SwiftUI
+
+@main
+struct StackedCardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
